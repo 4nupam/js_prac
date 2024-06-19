c57adfd2ed80e9c545c9a1c18f6f53640bfdaaea
4nupam <101491219+4nupam@users.noreply.github.com>
2024-06-19 15:10:25 +0530

    Add files via upload

---
+++ js projects/tabChange/script.js
@@ -0,0 +1,15 @@
+var btn = document.querySelectorAll('.inner_button');
+var divs = document.querySelectorAll('.inner_div');
+
+btn.forEach(t => {
+    t.addEventListener('click', () => {
+        const targetId = t.getAttribute('data-target');
+        const targetDiv = document.getElementById(targetId);
+
+        btn.forEach(button => button.classList.remove('active'));
+        divs.forEach(content => content.classList.remove('active'));
+
+        t.classList.add('active');
+        targetDiv.classList.add('active');
+    });
+});
